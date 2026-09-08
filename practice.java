6b011c698542e1f79b1f1005d22db13552c9f46c
Abdul Rehman <codewithabdul1010@gmail.com>
2026-09-08 20:42:46 +0530

    java question solve

--- practice.java
+++ practice.java
@@ -5,7 +5,12 @@ public class practice {
 
         // 1. A shirt costs $1500 and has a 20% discount. Calculate the discount amount and the final price
 
+        int shirtAmt = 1500;
+        int dis = 20;
+        
+        int disAmt = shirtAmt*dis/100;
 
+        System.out.println("shirtAmt: "+(shirtAmt-disAmt));
 
 
 
@@ -16,7 +21,10 @@ public class practice {
 
         // 2. Create variables for birth year and current year, then calculate and print the person's age.
 
+        int birthYr = 1998;
+        int currentYr = 2026;
 
+        System.out.println("age:" + (currentYr - birthYr));
 
 
 
@@ -26,6 +34,6 @@ public class practice {
 
         // 3. A Customer consumes 150 units of electricity at $6 per unit. Calculate and print the total electricity bill.
 
-
+        System.out.println( "total bill:" + (1500*6));
     }
 }
