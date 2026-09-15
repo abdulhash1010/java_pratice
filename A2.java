9e5139f18d75fc677c2269c563e9503be22ca4b6
Abdul Rehman <codewithabdul1010@gmail.com>
2026-09-15 23:23:47 +0530

    java practice

---
+++ A2.java
@@ -0,0 +1,56 @@
+public class A2 {
+    public static void main (String[]args){
+        int a = 39;
+        int b = 50;
+        int sum = (a + b);
+        int sub = (a - b);
+        int mul = (a * b);
+        int div = (a / b);
+        int mod = (a % b);
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
+
+
+        float f = 15.55f;
+        float g = 79.99f;
+        float add  = (f + g);
+        float subt  = (f - g);
+        float mult  = (f * g);
+        float dive  = (f / g);
+        float modu  = (f % g);
+
+        System.out.println(add);
+        System.out.println(subt);
+        System.out.println(mult);
+        System.out.println(dive);
+        System.out.println(modu);
+
+        int A2 = 50;
+        A2+=50;
+        System.out.println(A2);
+
+        float f2 = 34.55f;
+        f2-=23.77;
+        System.out.println(f2);
+
+        double d2 = 20.778787;
+        d2*=24.9898898;
+        System.out.println(d2);
+
+
+        long l2 = 8989898;
+        l2/=767676;
+        System.out.println(l2);
+
+
+       
+
+
+
+    }
+    
+}
