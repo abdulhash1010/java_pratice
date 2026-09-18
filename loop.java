af48549f27196f16c6a200a92d7f3b4de9af9f51
Abdul Rehman <codewithabdul1010@gmail.com>
2026-09-18 23:45:45 +0530

    java practice

---
+++ loop.java
@@ -0,0 +1,20 @@
+//for loop
+
+public class loop {
+    public static void main(String[]args){
+        for (int i = 1;  i<=10; i++){
+
+        
+        System.out.println(i);
+    }
+    
+
+//while loop
+
+int i = 1; 
+while (i<=10){
+    System.out.println(i);
+    i++;
+}
+    }
+}
