fd07b32d6ee48715ee1187c670ce28a150077daa
Abdul Rehman <codewithabdul1010@gmail.com>
2026-09-21 22:37:11 +0530

    prectice java

---
+++ loop2.java
@@ -0,0 +1,20 @@
+//for loop
+
+public class loop2 {
+    public static void main(String[]args){
+        for(int i= 1; i<=10; i++){
+            System.out.println(i);
+        }
+    
+    
+
+
+//while loop
+
+            int e= 1;
+            while(e<=10){
+             System.out.println(e);
+             e++;
+            }
+    }
+}        
