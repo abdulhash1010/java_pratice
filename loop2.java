bc5fbcac47d0c9e18ba61be8fbe4eb541a0e2775
Abdul Rehman <codewithabdul1010@gmail.com>
2026-09-24 19:17:21 +0530

    java practice

--- loop2.java
+++ loop2.java
@@ -1,20 +1,52 @@
 //for loop
 
-public class loop2 {
-    public static void main(String[]args){
-        for(int i= 1; i<=10; i++){
-            System.out.println(i);
+ public class loop2 {
+     public static void main(String[]args){
+         for(int i= 1; i<=10; i++){
+             System.out.println(i);
+         }
+    
+    
+
+
+// //while loop
+
+             int e= 1;
+             while(e<=10){
+              System.out.println(e);
+              e++;
+             }
+
+        
+
+
+// solving problems
+
+// ques 1:- 1 to 10 even no. pprint karo ?
+  
+
+        for(int a =0; a <=10; a=a+2){
+
+            System.out.println(a);
         }
     
-    
 
+// ques 2 :- 1 to 20 odd no. print kro?
 
-//while loop
+          for(int b=0; b <=10; b = b+3){
+            System.out.println(b);
+          }
+        
+     
 
-            int e= 1;
-            while(e<=10){
-             System.out.println(e);
-             e++;
-            }
+    //ques 3:- 1 to 10 ka sum print karo?
+
+          int sum = 0 ;
+          int i;
+          for( i =1; i<=10; i++){
+               sum = sum + i;
+            System.out.println(sum);
+          }
+        }
     }
-}        
+          
